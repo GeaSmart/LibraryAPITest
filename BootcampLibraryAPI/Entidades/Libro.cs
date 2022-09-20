750c33a845acf333023eb819aef5b53f52876929
Gerson Azabache <gerson_dunk@hotmail.com>
2022-09-20 09:12:08 -0500

    minor changes

--- BootcampLibraryAPI/Entidades/Libro.cs
+++ BootcampLibraryAPI/Entidades/Libro.cs
@@ -8,8 +8,6 @@ namespace BootcampLibraryAPI.Entidades
         [Required]
         [StringLength(75)]
         public string Titulo { get; set; }
-
         public DateTime FechaPublicacion { get; set; }
-
     }
 }
--- BootcampLibraryAPI/Program.cs
+++ BootcampLibraryAPI/Program.cs
@@ -10,12 +10,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     ));
 
 
-
-
-//var connectionString = builder.Configuration.GetConnectionString("defaultConnection");
-//builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
