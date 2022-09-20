9ea83e5f5e254e5dd00904dacb1687201c41dd08
Gerson Azabache <gerson_dunk@hotmail.com>
2022-09-20 10:00:15 -0500

    first two endpoints added in autores controller

---
+++ BootcampLibraryAPI/Controllers/AutoresController.cs
@@ -0,0 +1,36 @@
+﻿using BootcampLibraryAPI.Data;
+using BootcampLibraryAPI.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BootcampLibraryAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Autor>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Autor autor)
+        {
+            //TODO: No existan nombres duplicados
+
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
