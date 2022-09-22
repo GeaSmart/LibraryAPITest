b5cc90bb277cf2897769387a9ce9b8e0cd8a40b5
Gerson Azabache <gerson_dunk@hotmail.com>
2022-09-22 09:12:01 -0500

    automapper profile created and configured initially

---
+++ BootcampLibraryAPI/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BootcampLibraryAPI.DTO;
+using BootcampLibraryAPI.Entidades;
+
+namespace BootcampLibraryAPI.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            //Reglas de mapeo
+            CreateMap<Autor, AutorDTO>();
+            CreateMap<AutorCreacionDTO, Autor>();
+            CreateMap<Libro, LibroDTO>();
+            CreateMap<LibroCreacionDTO, Libro>();
+        }
+    }
+}
