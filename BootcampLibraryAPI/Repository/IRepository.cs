a7c003786dea479a8858d0f986e9e22b84cd1f3d
happytestinguser <usuario.correo.temporal@gmail.com>
2022-09-25 01:46:10 -0500

    repository pattern interface and class added

---
+++ BootcampLibraryAPI/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace BootcampLibraryAPI.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task PostAsync(T entity);
+        Task<List<T>> GetAsync();
+        Task<List<T>> GetAsync(Expression<Func<T, bool>> filter);
+        Task<T> GetAsync(int id);
+        Task PutAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ BootcampLibraryAPI/Repository/Repository.cs
@@ -0,0 +1,59 @@
+﻿using BootcampLibraryAPI.Data;
+using BootcampLibraryAPI.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BootcampLibraryAPI.Repository
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        public async Task<List<T>> GetAsync()
+        {
+            //return await context.Students.ToListAsync(); modo sin repository
+            return await context.Set<T>().ToListAsync();
+        }
+        public async Task<T> GetAsync(int id)
+        {
+            return await context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+        }
+        public async Task PostAsync(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            context.Add(entity);
+            await context.SaveChangesAsync();
+        }
+        public async Task PutAsync(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            context.Update(entity);
+            await context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await context.Set<T>().FindAsync(id);
+
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            context.Set<T>().Remove(entity);
+            await context.SaveChangesAsync();
+
+        }
+        public async Task<List<T>> GetAsync(Expression<Func<T, bool>> filter)
+        {
+            return await context.Set<T>().Where(filter).ToListAsync();
+        }
+    }
+}
